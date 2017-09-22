38732a27e11fe1aa1021dbd08afed43e59475500
Oussama Essafi <oussama-essafi@hotmail.com>
2017-09-22 15:28:12 +0200

    Initial commit

---
+++ src/main/java/com/servlets/MyServlet.java
@@ -0,0 +1,16 @@
+package com.servlets;
+
+import java.io.IOException;
+
+/**
+ * Created by Oussama on 22/09/2017.
+ */
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello motherfucker!</h2>
+</body>
+</html>
