dc4b5798fa2610c16071285a0ee867e15b4d128c
mcakirog <metin.cakiroglu@student.unamur.be>
2017-09-22 16:28:52 +0200

    premier essaie de cakiroglu

--- src/main/java/com/servlets/MyServlet.java
+++ src/main/java/com/servlets/MyServlet.java
@@ -6,6 +6,7 @@ import java.io.IOException;
  * Created by Oussama on 22/09/2017.
  */
 public class MyServlet extends javax.servlet.http.HttpServlet {
+
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello my friend!</h2>
+<h2>Hello my friend!,Hello motherer </h2>
 </body>
 </html>
